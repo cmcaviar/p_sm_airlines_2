e4ffee3220a46ad5f912f6244bd79ca0a5ad8cb1
Тицкий Александр Александрович <tankusttt@mail.ru>
2022-10-18 12:04:35 +0300

    add testcontainers

--- src/main/java/ru/kataproject/p_sm_airlines_1/entity/Dto/DestinationDTO.java
+++ src/main/java/ru/kataproject/p_sm_airlines_1/entity/Dto/DestinationDTO.java
@@ -2,12 +2,14 @@ package ru.kataproject.p_sm_airlines_1.entity.Dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ru.kataproject.p_sm_airlines_1.entity.Enumeration.CountryCode;
 
 import java.util.TimeZone;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class DestinationDTO {
 
     private Long id;
