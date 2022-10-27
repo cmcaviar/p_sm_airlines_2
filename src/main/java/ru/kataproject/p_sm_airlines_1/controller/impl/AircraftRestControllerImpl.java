6b63d8314763ea54da4d2221fb5223f7ff662993
asila2104 <79084814+asila2104@users.noreply.github.com>
2022-10-27 18:56:35 +0300

    add logging aircraft rest controller

--- src/main/java/ru/kataproject/p_sm_airlines_1/controller/impl/AircraftRestControllerImpl.java
+++ src/main/java/ru/kataproject/p_sm_airlines_1/controller/impl/AircraftRestControllerImpl.java
@@ -1,6 +1,7 @@
 package ru.kataproject.p_sm_airlines_1.controller.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class AircraftRestControllerImpl implements AircraftRestController {
 
     private final AircraftService aircraftService;
@@ -21,29 +23,34 @@ public class AircraftRestControllerImpl implements AircraftRestController {
 
     @Override
     public List<AircraftDTO> getAllAircrafts() {
+        log.info("getAllAircrafts()");
         return aircraftService.getAllAircrafts().stream().map(aircraftMapper::convertToDTO)
                 .collect(Collectors.toList());
     }
 
     @Override
     public AircraftDTO getAircraftById(@PathVariable final Long id) {
+        log.info("getAircraftById() with id = " + id);
         return aircraftMapper.convertToDTO(aircraftService.getAircraftById(id));
     }
 
     @Override
     public AircraftDTO saveAircraft(@RequestBody final AircraftDTO aircraftDTO) {
+        log.info("saveAircraft()");
         aircraftService.saveAircraft(aircraftMapper.convertToEntity(aircraftDTO));
         return aircraftDTO;
     }
 
     @Override
     public AircraftDTO updateAircraft(@RequestBody final AircraftDTO aircraftDTO) {
+        log.info("updateAircraft() with id = " + aircraftDTO.getId());
         aircraftService.updateAircraft(aircraftMapper.convertToEntity(aircraftDTO));
         return aircraftDTO;
     }
 
     @Override
     public void deleteAircraft(@PathVariable final Long id) {
+        log.info("deleteAircraft() with id = " + id);
         aircraftService.deleteAircraft(id);
     }
 }
