24447509391e6f13bf94f52a30dc1d248fb1e5fd
asila2104 <79084814+asila2104@users.noreply.github.com>
2022-11-01 00:46:24 +0300

    add ticket controller logging

--- src/main/java/ru/kataproject/p_sm_airlines_1/controller/impl/TicketControllerImpl.java
+++ src/main/java/ru/kataproject/p_sm_airlines_1/controller/impl/TicketControllerImpl.java
@@ -1,6 +1,7 @@
 package ru.kataproject.p_sm_airlines_1.controller.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,24 +16,28 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@Log4j2
 public class TicketControllerImpl implements TicketController {
 
     private final TicketService ticketService;
 
     @Override
     public ResponseEntity<TicketDTO> createTicket (final TicketDTO ticketDTO) {
+        log.info("createTicket()");
         TicketDTO savedTicket = ticketService.createTicket(ticketDTO);
         return new ResponseEntity<>(savedTicket, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<TicketDTO> updateTicket (final TicketDTO ticketDTO) {
+        log.info("updateTicket() with id = " + ticketDTO.getId());
         ticketService.updateTicket(ticketDTO);
         return new ResponseEntity<>(ticketDTO,HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<List<TicketDTO>> getListOfTickets (final Long id, final String reservationNumber) {
+        log.info("getListOfTickets() with id = " + id + " and reservationNumber = " + reservationNumber);
         return new ResponseEntity<>(ticketService.getTickets(id,reservationNumber),HttpStatus.OK);
     }
 }
